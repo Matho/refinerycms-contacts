29355a5aec1fedd49ab7b708b07a9024e83fb8ac
Martin Markech <martin.markech@matho.sk>
2012-12-22 14:48:15 +0100

    add email obfuscation - converting to decimal entities

--- app/models/refinery/contacts/mail.rb
+++ app/models/refinery/contacts/mail.rb
@@ -8,6 +8,18 @@ module Refinery
       acts_as_indexed :fields => [:mail]
 
       validates_format_of :mail, :with => /.+@.+\..+/i , :allow_blank => false
+
+      def self.obfuscated
+        self.all.each do |m|
+          m.mail = self.to_decimal_entities(m.mail).html_safe
+        end
+      end
+
+      private
+      def self.to_decimal_entities(string)
+         string.each_char.collect {|char| "&##{char.unpack('U')[0]};" }.join
+      end
+
     end
   end
 end
--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
+## 0.1.1 [22 December 2012]
+* add email obfuscation in selectbox
+
 ## 0.1.0 [21 December 2012]
 * initial commit
